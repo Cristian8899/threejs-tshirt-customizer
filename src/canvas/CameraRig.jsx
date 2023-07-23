cb0a264761c4ddd91ecc10c62038815aa37dcc1f
Cristian Pitu <cristian.pitu88@gmail.com>
2023-07-23 23:13:51 +0300

    first commit

---
+++ src/canvas/CameraRig.jsx
@@ -0,0 +1,48 @@
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+const CameraRig = ({ children }) => {
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  useFrame((state, delta) => {
+
+    const isBreakpoint = window.innerWidth <=1260;
+    const isMobile = window.innerWidth <=600;
+
+    //set the inital position of the model
+
+    let tagretPosition = [-0.4,0,2];
+    if (snap.intro) {
+        if (isBreakpoint) tagretPosition= [0,0,2];
+        if (isMobile) tagretPosition=[0,0.2,2.5];
+    } else{
+        if (isMobile) tagretPosition=[0,0,2.5]
+        else 
+        tagretPosition =[0,0,2];
+    }
+
+//set model camera psoition
+
+easing.damp3(state.camera.position, tagretPosition,0.25,delta);
+
+
+
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
+
+  //set the model rotation smoothly
+
+  return <group ref={group}>{children}</group>;
+};
+
+export default CameraRig;
